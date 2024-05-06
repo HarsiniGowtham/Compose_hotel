1ab5d719e4600f8367ea2517d79bb88ec779ca64
harsini-tec <harsini.g@teclever.com>
2024-05-06 20:19:05 +0530

    first commit

---
+++ app/src/main/java/com/hgh/hotelcompose/screen/ScreenMain.kt
@@ -0,0 +1,18 @@
+package com.hgh.hotelcompose.screen
+
+import Routes
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun ScreenMain(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.Login.route){
+        composable(Routes.Login.route){
+            LoginPage(navController=navController)
+        }
+    }
+
+}
